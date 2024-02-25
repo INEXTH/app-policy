c03b5d57ab076a6e0e516c5bb5aea1aa24d52be6
Akexorcist <akexorcist@gmail.com>
2024-02-25 17:48:09 +0700

    Add Github Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/inex-app-policy",
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: true,
+  };
 
 export default nextConfig;
