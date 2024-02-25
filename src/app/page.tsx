e66a935ddc26f9e89a87b9727a2b07f5f70a235a
Akexorcist <akexorcist@gmail.com>
2024-02-25 17:55:23 +0700

    Fix incorrect page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,41 @@ export default function Home() {
     <main className="flex min-h-screen flex-col justify-between p-24 items-center bg-white text-gray-800">
       <div className="max-w-7xl">
         <div className="text-4xl">Privacy Policy for Android Application of Innovative Experiment Co.,Ltd.</div>
-        <a href="./analog-monitoring"><div className="text-xl mt-8">• Analog Monitoring</div></a>
-        <a href="./blue-music-box"><div className="text-xl mt-4">• Blue Music Box</div></a>
-        <a href="./bluestick-control"><div className="text-xl mt-4">• BlueStick Control [Phone]</div></a>
-        <a href="./bluetooth-control"><div className="text-xl mt-4">• Bluetooth Control [Tablet]</div></a>
-        <a href="./bluetooth-direct"><div className="text-xl mt-4">• Bluetooth Direct</div></a>
-        <a href="./data-sci"><div className="text-xl mt-4">• DataSci</div></a>
-        <a href="./droid2droid-controller"><div className="text-xl mt-4">• Droid2Droid Controller</div></a>
-        <a href="./ioio-avt-test"><div className="text-xl mt-4">• IOIO AVT Test</div></a>
-        <a href="./ioio-bot"><div className="text-xl mt-4">• IOIO Bot</div></a>
-        <a href="./ioio-camera-360"><div className="text-xl mt-4">• IOIO Camera 360</div></a>
-        <a href="./ioio-camera-robot"><div className="text-xl mt-4">• IOIO Camera Robot</div></a>
-        <a href="./ioio-example"><div className="text-xl mt-4">• IOIO Example</div></a>
-        <a href="./ioio-inventor"><div className="text-xl mt-4">• IOIO Inventor</div></a>
-        <a href="./ioio-q-analog-reader"><div className="text-xl mt-4">• IOIO-Q Analog Reader</div></a>
-        <a href="./ioio-q-humidity"><div className="text-xl mt-4">• IOIO-Q Humidity</div></a>
-        <a href="./ioio-q-io-basic"><div className="text-xl mt-4">• IOIO-Q IO Basic</div></a>
-        <a href="./ioio-q-ir-range-sensor"><div className="text-xl mt-4">• IOIO-Q IR Range Sensor</div></a>
-        <a href="./ioio-q-led-brightness"><div className="text-xl mt-4">• IOIO-Q LED Brightness</div></a>
-        <a href="./ioio-q-piezo-piano"><div className="text-xl mt-4">• IOIO-Q Piezo Piano</div></a>
-        <a href="./ioio-q-project"><div className="text-xl mt-4">• IOIO-Q Project</div></a>
-        <a href="./ioio-q-rgb-led-ca"><div className="text-xl mt-4">• IOIO-Q RGB LED [CA]</div></a>
-        <a href="./ioio-q-rgb-led-cc"><div className="text-xl mt-4">• IOIO-Q RGB LED [CC]</div></a>
-        <a href="./ioio-q-shift-register"><div className="text-xl mt-4">• IOIO-Q Shift Register</div></a>
-        <a href="./ioio-segment"><div className="text-xl mt-4">• IOIO Segment</div></a>
-        <a href="./ipst-daq"><div className="text-xl mt-4">• IPST-DAQ</div></a>
-        <a href="./lcd-controller"><div className="text-xl mt-4">• LCD Controller</div></a>
-        <a href="./lm3914-calculation"><div className="text-xl mt-4">• LM3914 Calculation</div></a>
-        <a href="./loadswitch-4"><div className="text-xl mt-4">• LoadSwitch-4</div></a>
-        <a href="./lux-meter"><div className="text-xl mt-4">• Lux Meter</div></a>
-        <a href="./rfid-time-recorder"><div className="text-xl mt-4">• RFID Time Recorder</div></a>
-        <a href="./take-snapshot"><div className="text-xl mt-4">• Take Snapshot</div></a>
-        <a href="./tpa-2013-scoreboard"><div className="text-xl mt-4">• TPA 2013 Scoreboard</div></a>
-        <a href="./uart-rfid-reader"><div className="text-xl mt-4">• UART RFID Reader</div></a>
-        <a href="./wifly-remote"><div className="text-xl mt-4">• WiFly Remote</div></a>
-        <a href="./wifly-switch-controller"><div className="text-xl mt-4">• WiFly Switch Controller</div></a>
+        <a href="inex-app-policy/analog-monitoring"><div className="text-xl mt-8">• Analog Monitoring</div></a>
+        <a href="inex-app-policy/blue-music-box"><div className="text-xl mt-4">• Blue Music Box</div></a>
+        <a href="inex-app-policy/bluestick-control"><div className="text-xl mt-4">• BlueStick Control [Phone]</div></a>
+        <a href="inex-app-policy/bluetooth-control"><div className="text-xl mt-4">• Bluetooth Control [Tablet]</div></a>
+        <a href="inex-app-policy/bluetooth-direct"><div className="text-xl mt-4">• Bluetooth Direct</div></a>
+        <a href="inex-app-policy/data-sci"><div className="text-xl mt-4">• DataSci</div></a>
+        <a href="inex-app-policy/droid2droid-controller"><div className="text-xl mt-4">• Droid2Droid Controller</div></a>
+        <a href="inex-app-policy/ioio-avt-test"><div className="text-xl mt-4">• IOIO AVT Test</div></a>
+        <a href="inex-app-policy/ioio-bot"><div className="text-xl mt-4">• IOIO Bot</div></a>
+        <a href="inex-app-policy/ioio-camera-360"><div className="text-xl mt-4">• IOIO Camera 360</div></a>
+        <a href="inex-app-policy/ioio-camera-robot"><div className="text-xl mt-4">• IOIO Camera Robot</div></a>
+        <a href="inex-app-policy/ioio-example"><div className="text-xl mt-4">• IOIO Example</div></a>
+        <a href="inex-app-policy/ioio-inventor"><div className="text-xl mt-4">• IOIO Inventor</div></a>
+        <a href="inex-app-policy/ioio-q-analog-reader"><div className="text-xl mt-4">• IOIO-Q Analog Reader</div></a>
+        <a href="inex-app-policy/ioio-q-humidity"><div className="text-xl mt-4">• IOIO-Q Humidity</div></a>
+        <a href="inex-app-policy/ioio-q-io-basic"><div className="text-xl mt-4">• IOIO-Q IO Basic</div></a>
+        <a href="inex-app-policy/ioio-q-ir-range-sensor"><div className="text-xl mt-4">• IOIO-Q IR Range Sensor</div></a>
+        <a href="inex-app-policy/ioio-q-led-brightness"><div className="text-xl mt-4">• IOIO-Q LED Brightness</div></a>
+        <a href="inex-app-policy/ioio-q-piezo-piano"><div className="text-xl mt-4">• IOIO-Q Piezo Piano</div></a>
+        <a href="inex-app-policy/ioio-q-project"><div className="text-xl mt-4">• IOIO-Q Project</div></a>
+        <a href="inex-app-policy/ioio-q-rgb-led-ca"><div className="text-xl mt-4">• IOIO-Q RGB LED [CA]</div></a>
+        <a href="inex-app-policy/ioio-q-rgb-led-cc"><div className="text-xl mt-4">• IOIO-Q RGB LED [CC]</div></a>
+        <a href="inex-app-policy/ioio-q-shift-register"><div className="text-xl mt-4">• IOIO-Q Shift Register</div></a>
+        <a href="inex-app-policy/ioio-segment"><div className="text-xl mt-4">• IOIO Segment</div></a>
+        <a href="inex-app-policy/ipst-daq"><div className="text-xl mt-4">• IPST-DAQ</div></a>
+        <a href="inex-app-policy/lcd-controller"><div className="text-xl mt-4">• LCD Controller</div></a>
+        <a href="inex-app-policy/lm3914-calculation"><div className="text-xl mt-4">• LM3914 Calculation</div></a>
+        <a href="inex-app-policy/loadswitch-4"><div className="text-xl mt-4">• LoadSwitch-4</div></a>
+        <a href="inex-app-policy/lux-meter"><div className="text-xl mt-4">• Lux Meter</div></a>
+        <a href="inex-app-policy/rfid-time-recorder"><div className="text-xl mt-4">• RFID Time Recorder</div></a>
+        <a href="inex-app-policy/take-snapshot"><div className="text-xl mt-4">• Take Snapshot</div></a>
+        <a href="inex-app-policy/tpa-2013-scoreboard"><div className="text-xl mt-4">• TPA 2013 Scoreboard</div></a>
+        <a href="inex-app-policy/uart-rfid-reader"><div className="text-xl mt-4">• UART RFID Reader</div></a>
+        <a href="inex-app-policy/wifly-remote"><div className="text-xl mt-4">• WiFly Remote</div></a>
+        <a href="inex-app-policy/wifly-switch-controller"><div className="text-xl mt-4">• WiFly Switch Controller</div></a>
       </div>
     </main>
   );
